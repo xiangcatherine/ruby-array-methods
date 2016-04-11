ddc315efa9602e7d986e9257a08ef62847e934c9
Antony Donovan <antony.donovan@generalassemb.ly>
2016-04-11 14:38:55 -0400

    Follow ruby filename conventions
    close #6

--- spec/example.spec.js
+++ spec/example.spec.js
@@ -1,35 +1,9 @@
 'use strict';
 
-var ins = require('../lib/ins.js');
+var example = require('../lib/example');
 
-describe('product', function() {
-
-  it('returns 1 when called without arguments', function() {
-    expect(ins.product()).toBe(1);
+describe('Example', () => {
+  it('is true', () => {
+    expect(example()).toBe(true);
   });
-
-  it('returns the argument when called with just one', function() {
-    expect(ins.product(42)).toBe(42);
-  });
-
-  it('returns the product of all the arguments', function() {
-    expect(ins.product(1, 2, 3, 4, 5)).toBe(120);
-  });
-
-});
-
-describe('max', function() {
-
-  it('returns undefined when called without arguments', function() {
-    expect(ins.max()).toBeUndefined();
-  });
-
-  it('returns the argument when called with just one', function() {
-    expect(ins.max(42)).toBe(42);
-  });
-
-  it('returns the maximum of all the arguments', function() {
-    expect(ins.max(-1, -2, -3, -4, -5)).toBe(-1);
-  });
-
 });
