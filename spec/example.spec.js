f450ea6786d222e84e3ee63cb5ede106c80ede90
Jeffrey Horn <jeffh@generalassemb.ly>
2016-06-08 09:36:22 -0400

    Update from node-template

--- spec/example.spec.js
+++ spec/example.spec.js
@@ -1,9 +1,34 @@
 'use strict';
 
-var example = require('../lib/example');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
 
-describe('Example', () => {
-  it('is true', () => {
-    expect(example()).toBe(true);
+chai.use(chaiAsPromised);
+
+const expect = chai.expect;
+
+const example = require('../lib/example');
+
+describe('Sync', function () {
+  it('is true', function () {
+    return expect(example.sync(true)).to.be.true;
+  });
+});
+
+describe('Async', function () {
+  it('is true', function (done) {
+    example.async(true, function (error, value) {
+      if (error || value !== true) {
+        error = error || new Error(`value is ${value}`);
+      }
+
+      done(error);
+    });
+  });
+});
+
+describe('Promise', function () {
+  it('is true', function () {
+    return expect(example.promise(true)).to.eventually.be.true;
   });
 });
