5bc68784c59a4405cec0f8144845f2038a9a634e
racheltstevens <racheltstevens@gmail.com>
2016-11-01 16:28:02 -0400

    Remove .js files from lib/ bin/ and spec/
    Updated for not using js in this lesson.

--- bin/people-array.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-let loadPeople = require('../lib/people.js');
-
-loadPeople().then((people) => {
-  let count = people.reduce((m /*, p */) => m + 1, 0);
-  console.log(`count: ${count}, length: ${people.length}`);
-}).catch((error) => console.log(error));
