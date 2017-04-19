17a69d5d410af1b2b1fcbe34fc22c28ddfaebfdd
xiangcatherine <xiang.catherine@gmail.com>
2017-04-19 12:22:08 -0400

    ruby array methods training, fibonacci

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -2,4 +2,40 @@
 
 fibs = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 
-p fibs
+# Way 1
+prod_fibs =
+  fibs.slice(1, fibs.length).reduce(1) do |accumulator, element|
+    accumulator * element
+  end
+puts prod_fibs
+
+# Way 2
+prod_fibs =
+  fibs.select(&:positive?).reduce(1) do |accumulator, element|
+    accumulator * element
+  end
+puts prod_fibs
+
+# Way 1
+sum_odd_fibs =
+  fibs.select(&:odd?).reduce(0) do |accumulator, element|
+    # or fibs.select(&:odd?).reduce(:+)
+    accumulator + element
+  end
+puts sum_odd_fibs
+
+# Way 1
+prod_even_fibs =
+  fibs.select(&:even?).select(&:positive?).reduce(1) do |accumulator, element|
+    accumulator * element
+  end
+puts prod_even_fibs
+
+# Way 2
+prod_even_fibs =
+  fibs.slice(1, fibs.length).select(&:even?).reduce(1) do |accumulator, element|
+    accumulator * element
+  end
+puts prod_even_fibs
+
+puts fibs
